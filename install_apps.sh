0e0e62ec02521adcd7d130b6380c055d338c67bc
magniks <53019044+magniks@users.noreply.github.com>
2025-06-16 10:55:55 +0200

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -3,6 +3,9 @@
 echo "Running apt update"
 sudo apt-get update -y
 
+echo "Running apt upgrade"
+sudo apt-get upgrade -y
+
 echo "Installing nmap"
 sudo apt install nmap -y
 
@@ -30,9 +33,25 @@ sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt update -y
 sudo apt install openjdk-21-jdk -y
 
-download_url="https://portswigger.net/burp/releases/download?product=community&type=jar"
 echo "Installing Burp Suite"
+download_url="https://portswigger.net/burp/releases/download?product=community&type=jar"
 wget --output-document=burpsuite_community.jar "$download_url"
 
 echo "Installing Oracle Developer VScode extension"
 runuser -l azureadmin -c 'code --install-extension Oracle.oracledevtools --force'
+
+echo "Installing git"
+sudo apt install git -y
+
+echo "Installing python"
+sudo apt install python -y
+sudo apt install python3 -y
+
+echo "Installing mitm proxy"
+sudo apt install mitmproxy -y
+
+echo "Setting up ubuntu desktop"
+sudo apt install xrdp -y
+sudo systemctl enable xrdp
+sudo systemctl start xrdp
+sudo apt install ubuntu-desktop -y
